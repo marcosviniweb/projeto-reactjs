eea66d54e43e4c7d00cf38e0cfa13c321e613cbb
marcosviniweb <63163857+marcosviniweb@users.noreply.github.com>
2023-08-20 21:19:29 -0300

    feat(wip): search and datatable

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export function SearchBar() {
+export function SearchBar(props) {
+  const [localSearchQuery, setLocalSearchQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setLocalSearchQuery(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    props.onSearch(localSearchQuery); // Notificar o componente pai sobre a consulta de pesquisa
+  };
+
   return (
     <div className="input-group mb-3">
       <input
         type="text"
+        value={localSearchQuery}
+        onChange={handleInputChange}
         className="form-control"
         placeholder=""
-        aria-label="Example text with button addon"
+        aria-label="Texto da pesquisa"
         aria-describedby="button-addon1"
       />
       <button
         className="btn btn-outline-secondary"
         type="button"
         id="button-addon1"
+        onClick={handleSearchClick} // Adicionado manipulador de clique ao botão
       >
         Pesquisar
       </button>
